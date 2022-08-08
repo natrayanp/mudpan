271b685c5228aeaedd38046cde0cb631dd7e4226
natrayan <natrayanp@gmail.com>
2022-08-09 00:58:40 +0800

    conf

--- apps/fe/src/environments/environment.prod.ts
+++ apps/fe/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  config : {
+    apiKey: "AIzaSyB0Shy7aNh93SuDGFSOky_0Znt4W-nUVd4",
+    authDomain: "natauth-c532d.firebaseapp.com",
+    databaseURL: "https://natauth-c532d.firebaseio.com",
+    projectId: "natauth-c532d",
+    storageBucket: "natauth-c532d.appspot.com",
+    messagingSenderId: "824531220934",        
+  }
 };
